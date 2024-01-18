448c6cd548b8286e7dfa7cc066fac5252e46e6a1
[Leevi Rinnetmäki] <leevijr@metropolia.fi>
2024-01-18 13:01:13 +0200

    testi

---
+++ tehtavia.py
@@ -0,0 +1,54 @@
+import random
+
+valinta=int(input("tehtävä"))
+
+
+if valinta==1:
+    nimi=input("Mikä on sun nimi? :)")
+    print("Moi",nimi)
+
+elif valinta==2:
+    sade=int(input("Säde?"))
+    sade=sade*sade*3.14
+    print("Ympyrän säde on",sade)
+
+elif valinta==3:
+    kanta=int(input("kanta?"))
+    korkeus=int(input("korkeus?"))
+    area=kanta*korkeus
+    perimiter=kanta*2+korkeus*2
+
+    print("pinta ala on", area, "ja piiri on", perimiter)
+
+elif valinta==4:
+    eka=int(input("eka?"))
+    toka=int(input("toka?"))
+    kolmas=int(input("kolmas?"))
+
+    sum=eka+toka+kolmas
+    product=eka*toka*kolmas
+    mean=sum/3
+    print("summa on",sum,"ja tulo on", product, "ja keskiarvo on", mean)
+
+elif valinta==5:
+    g=1
+    kg=1000
+    luoti=13.3*g
+    naula=32*luoti
+    leiviska=20*naula
+
+    leiviskäInput=float(input("leiviskät?"))
+    naulaInput=float(input("naulat?"))
+    luotiInput=float(input("luodit?"))
+
+    mass=leiviskäInput*leiviska+naulaInput*naula+luotiInput*luoti
+    masskg=int(mass/1000)
+    massg=mass-masskg*1000
+
+    print("Massa nykymittojen mukaan on",masskg, "kiloa ja", massg,"grammaa" )
+
+elif valinta==6:
+
+    kol=str(random.randint(0,9))+str(random.randint(0,9))+str(random.randint(0,9))
+    nel=str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6))
+    print(kol,nel)
